9f9ffd1b501a8adc9f049cac0759d71b35917819
Ryota7hos1 <rh1524@ic.ac.uk>
2025-11-28 11:45:06 +0000

    changed alu file to accomodate for the changes in alu_ctrl signal from the control unit (half way done)

--- rtl/alu/alu.sv
+++ rtl/alu/alu.sv
@@ -3,32 +3,27 @@ module alu #(
 ) (
     input  logic [D_WIDTH-1:0] aluop1,
     input  logic [D_WIDTH-1:0] aluop2,
-    input  logic [2:0]         aluctrl,
+    input  logic [3:0]         aluctrl,
     output logic [D_WIDTH-1:0] aluout,
     output logic               eq
 );
 
 always_comb begin
-    case (aluctrl)
-        3'b0: begin//add
+    casez (aluctrl)
+        4'b0: begin//add
             aluout = aluop1 + aluop2;
-            eq = 1'b0;
         end
-        3'b1: begin //subtract
+        4'b1: begin //subtract
             aluout = aluop1 - aluop2;
-            eq = aluout == 32'b0;
         end
-        3'b10: begin //and
+        4'b10: begin //and
             aluout = aluop1&aluop2;
-            eq = 1'b0;
         end
-        3'b11: begin //or
+        4'b11: begin //or
             aluout = aluop1 | aluop2;
-            eq = 1'b0;
         end
-        3'b101: begin //set less than
+        4'b101: begin //set less than
             aluout = $signed(aluop1) < $signed(aluop2);
-            eq1 = 1'b0;
         end
         default: begin
             aluout = 32'b0;
@@ -36,5 +31,11 @@ always_comb begin
         end
     endcase
 end
+
+always_comb begin
+    case (alu_ctrl)
+        
+end
+
 
 endmodule
