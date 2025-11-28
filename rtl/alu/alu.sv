ce4d44cac8de7eab74b92839923454aa40ad5cf5
Ryota7hos1 <rh1524@ic.ac.uk>
2025-11-28 16:38:11 +0000

    added all instructions needed for f1 task (single cycle)

--- rtl/alu/alu.sv
+++ rtl/alu/alu.sv
@@ -48,6 +48,27 @@ end
 
 always_comb begin
     case (alu_ctrl)
+        4'b0001: begin //subtract
+            eq = (aluop1 - aluop2) == 32'b0;
+        end
+        4'b0100: begin //xor
+            eq = (aluop1 ^ aluop2) == 32'b1;
+        end
+        4'b0010: begin //and
+            eq = (aluop1 & aluop2) == 32'b1;
+        end
+        4'b0011: begin //or
+            eq = (aluop1 | aluop2) == 32'b1;
+        end
+        4'b0101: begin //set less than
+            eq = ($signed(aluop1) < $signed(aluop2)) == 32'b1;
+        end
+        4'b0110: begin //set less than unsigned
+            eq = (aluop1 < aluop2) == 32'b1;
+        end
+        default: begin
+            eq = 1'b0;
+        end
     endcase
 end
 
