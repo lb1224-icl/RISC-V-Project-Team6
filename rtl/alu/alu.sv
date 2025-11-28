d9d28ef18a04bcdad5c19957bf7018728816290a
Ryota7hos1 <rh1524@ic.ac.uk>
2025-11-28 16:14:32 +0000

    added more instructions and introduced new architecture for alu

--- rtl/alu/alu.sv
+++ rtl/alu/alu.sv
@@ -10,31 +10,44 @@ module alu #(
 
 always_comb begin
     casez (aluctrl)
-        4'b0: begin//add
+        4'b0000: begin//add
             aluout = aluop1 + aluop2;
         end
-        4'b1: begin //subtract
+        4'b0001: begin //subtract
             aluout = aluop1 - aluop2;
         end
-        4'b10: begin //and
-            aluout = aluop1&aluop2;
+        4'b0111: begin //shift left by x
+            aluout = aluop1 << aluop2;
         end
-        4'b11: begin //or
+        4'b1000: begin //shift right by x
+            aluout = aluop1 >> aluop2;
+        end
+        4'b0100: begin //xor
+            aluout = aluop1 ^ aluop2;
+        end
+        4'b0010: begin //and
+            aluout = aluop1 & aluop2;
+        end
+        4'b0011: begin //or
             aluout = aluop1 | aluop2;
         end
-        4'b101: begin //set less than
+        4'b0101: begin //set less than
             aluout = $signed(aluop1) < $signed(aluop2);
+        end
+        4'b0110: begin //set less than unsigned
+            aluout = aluop1 < aluop2;
+        end
+        4'b1001: begin //shift right arthimetic
+            aluout = $signed(aluop1) >>> aluop2;
         end
         default: begin
             aluout = 32'b0;
-            eq = 1'b0;
         end
     endcase
 end
 
 always_comb begin
     case (alu_ctrl)
-    
     endcase
 end
 
