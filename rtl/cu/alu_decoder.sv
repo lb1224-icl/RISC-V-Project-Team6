e66ea2f568bc20841a65756d714a49c68ac94135
EthanCann <ethan.cann44@gmail.com>
2025-11-28 14:21:23 +0000

    the logic for the basic cpu instructions have all been implemented and complete

--- rtl/cu/alu_decoder.sv
+++ rtl/cu/alu_decoder.sv
@@ -13,29 +13,44 @@ module alu_decoder #(
 always_comb
 case (alu_op)       // to determine the ALU operation type
 
-    2'b00:   alu_ctrl = 3'b000;     // memory addressing calucations e.g. lw, sw  
+    2'b0:   alu_ctrl = 4'b0;     // memory addressing calucations e.g. lw, sw  
 
-    2'b01:   alu_ctrl = 3'b001;     // B-type ins
+    2'b1:   alu_ctrl = 4'b1;     // B-type ins
 
-    2'b10:  if (funct3 == 3'b000)   // R-type ins and other logical/arithemetic based instructions
-                if ({opcode_5, funct7_5} == 2'b11) 
-                    alu_ctrl = 3'b001;      // sub
+    2'b10:  if (funct3 == 3'b0)   // R-type ins and other logical/arithemetic based instructions
+                if (funct7_5 == 0) 
+                    alu_ctrl = 4'b0;     // add
                 else
-                    alu_ctrl = 3'b000;      // add
+                    alu_ctrl = 4'b1;     // sub
+
+            else if (funct3 == 3'b1)
+                alu_ctrl = 4'b111;        // logical shift left
             
-            else if (funct3 == 3'b010)
-                alu_ctrl = 3'b101;          // set less than
+            else if (funct3 == 3'b10)
+                alu_ctrl = 4'b101;       // set less than
+
+            else if (funct3 == 3'b11)
+                alu_ctrl = 4'b110;       // set less than unsigned
+
+            else if (funct3 == 3'b100)
+                alu_ctrl = 4'b100;       // xor
+            
+            else if (funct3 == 3'b101)
+                if (funct7_5 == 0)
+                    alu_ctrl = 4'b1000;     // logical shift right
+                else
+                    alu_ctrl = 4'b1001;     // arithmetic shift right
 
             else if (funct3 == 3'b110)
-                alu_ctrl = 3'b011;          // or
+                alu_ctrl = 4'b11;        // or
             
             else if (funct3 == 3'b111)
-                alu_ctrl = 3'b010;          // and
+                alu_ctrl = 4'b10;        // and
 
             else
-                //include error statememt that no such funct3 operation exists
+               $error("ALU_Decoder Error: funct3 out of range!");
     
-    default: // error message that no such alu_op exists
+    default: $error("ALU_Decoder Error: alu_op out of range!");
 
 endcase
 
