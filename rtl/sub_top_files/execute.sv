678b8dafbe92c539c1a5dbb76a318f4bd547e451
Ryota7hos1 <rh1524@ic.ac.uk>
2025-12-01 00:48:56 +0000

    finalized execute sub system with data size all implemented

--- rtl/sub_top_files/execute.sv
+++ rtl/sub_top_files/execute.sv
@@ -1,42 +1,57 @@
 module execute #(
     parameter D_WIDTH = 32
 ) (
-    input logic [D_WIDTH-1:0] rd1,
-    input logic [D_WIDTH-1:0] rd2,
-    input logic [D_WIDTH-1:0] pc,
-    input logic [D_WIDTH-1:0] imm_Ext,
-    input logic [2:0] alu_ctrl,
-    input logic alu_src,
-    output logic [D_WIDTH-1:0] alu_result,
+    input logic reg_write_e_i,
+    input logic [1:0] result_src_e_i,
+    input logic mem_write_e_i,
+    input logic [2:0] alu_ctrl_e,
+    input logic alu_src_e,
+    input logic [D_WIDTH-1:0] rd1_e,
+    input logic [D_WIDTH-1:0] rd2_e,
+    input logic [D_WIDTH-1:0] pc_e,
+    input logic [4:0] rd_e_i,
+    input logic [D_WIDTH-1:0] imm_ext_e,
+    input logic [D_WIDTH-1:0] pc_plus_4_e_i,
     output logic zero, //eq
-    output logic [D_WIDTH-1:0] write_data,
-    output logic [D_WIDTH-1:0] pc_target
+    output logic reg_write_e_o,
+    output logic [1:0] result_src_e_o,
+    output logic mem_write_e_o,
+    output logic [D_WIDTH-1:0] alu_result,
+    output logic [D_WIDTH-1:0] write_data_e,
+    output logic [4:0] rd_e_o,
+    output logic [D_WIDTH-1:0] pc_plus_4_e_o,
+    output logic [D_WIDTH-1:0] pc_target_e
 );
 
-logic [D_WIDTH-1:0] src_b;
+logic [D_WIDTH-1:0] src_b_e;
 
 alu ALU (
-    .aluop1(rd1),
-    .aluop2(src_b),
-    .aluctrl(alu_ctrl),
+    .aluop1(rd1_e),
+    .aluop2(src_b_e),
+    .aluctrl(alu_ctrl_e),
     .aluout(alu_result),
     .eq(zero)
 );
 
 mux_2 imm_mux (
-    .in0(rd2),
-    .in1(imm_Ext),
-    .sel(alu_src),
-    .out(src_b)
+    .in0(rd2_e),
+    .in1(imm_ext_e),
+    .sel(alu_src_e),
+    .out(src_b_e)
 );
 
 pc_branch #(.WIDTH(D_WIDTH)) u_branch (
-    .pc(pc),
-    .imm(imm_Ext),
-    .branch_pc(pc_target)
+    .pc(pc_e),
+    .imm(imm_ext_e),
+    .branch_pc(pc_target_e)
 );
 
 
-assign write_data = rd2;
+assign reg_write_e_o = reg_write_e_i;
+assign result_src_e_o = result_src_e_i;
+assign mem_write_e_o = mem_write_e_i;
+assign write_data_e = rd2_e;
+assign rd_e_o = rd_e_i;
+assign pc_plus_4_e_o = pc_plus_4_e_i;
 
 endmodule
