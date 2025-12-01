2ce8e10b23bb34e59be4a23afbdebaac7517f117
EthanCann <ethan.cann44@gmail.com>
2025-12-01 12:15:04 +0000

    fixes

--- rtl/alu/mux_4.sv
+++ rtl/alu/mux_4.sv
@@ -8,14 +8,14 @@ module mux_4 #(
     input  logic [1:0]         sel,
     output logic [D_WIDTH-1:0] out
 );
-    always_comb begin
-        case (sel)
-        2'b00: out = in0;
-        2'b01: out = in1;
-        2'b10: out = in2;
-        2'b11: out = in3;
-        default: out = '0;
-        endcase
-    end
+always_comb
+case (sel)
+    2'b00: out = in0;
+    2'b01: out = in1;
+    2'b10: out = in2;
+    2'b11: out = in3;
+    default: out = '0;
+endcase
+
 
 endmodule
--- rtl/sub_top_files/writeback.sv
+++ rtl/sub_top_files/writeback.sv
@@ -1,18 +1,18 @@
 module writeback # (
     parameter D_WIDTH = 32
 ) (
-    input logic reg_write_w_i, //we3
-    input logic [1:0] result_src_w, 
-    input logic [D_WIDTH-1] alu_result_w,
-    input logic [D_WIDTH-1] read_data_w,
-    input logic [4:0] rd_w_i,
-    input logic [D_WIDTH-1:-0] pc_plus4,
-    output logic reg_write_w_o,
-    output logic [4:0] rd_w_o,
-    output logic [D_WIDTH-1:0] result_w
+    input logic                   reg_write_w_i, //we3
+    input logic  [1:0]            result_src_w, 
+    input logic  [D_WIDTH-1]      alu_result_w,
+    input logic  [D_WIDTH-1]      read_data_w,
+    input logic  [4:0]            rd_w_i,
+    input logic  [D_WIDTH-1:-0]   pc_plus4,
+    output logic                  reg_write_w_o,
+    output logic [4:0]            rd_w_o,
+    output logic [D_WIDTH-1:0]    result_w
 )
 
-assign reg_write_w_i = reg_write_w_o
+assign reg_write_w_i = reg_write_w_o;
 assign rd_w_o = rd_w_i;
 
 mux_4 writeback_mux (
@@ -23,3 +23,5 @@ mux_4 writeback_mux (
     .sel(result_src_w),
     .out(result_w)
 )
+
+endmodule
