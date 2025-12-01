bd060f99748c45c3fc3dcdd0de65bf4e8ee42f4a
Ryota7hos1 <rh1524@ic.ac.uk>
2025-12-01 00:18:48 +0000

    Added size of arrays

--- rtl/sub_top_files/writeback.sv
+++ rtl/sub_top_files/writeback.sv
@@ -1,15 +1,15 @@
 module writeback # (
     parameter D_WIDTH = 32
 ) (
-    input logic reg_write_w_i,
-    input logic result_src_w,
-    input logic alu_result_w,
-    input logic read_data_w,
-    input logic rd_w_i,
-    input logic pc_plus4,
+    input logic reg_write_w_i, //we3
+    input logic [1:0] result_src_w, 
+    input logic [D_WIDTH-1] alu_result_w,
+    input logic [D_WIDTH-1] read_data_w,
+    input logic [4:0] rd_w_i,
+    input logic [D_WIDTH-1:-0] pc_plus4,
     output logic reg_write_w_o,
-    output logic rd_w_o,
-    output logic result_w
+    output logic [4:0] rd_w_o,
+    output logic [D_WIDTH-1:0] result_w
 )
 
 assign reg_write_w_i = reg_write_w_o
