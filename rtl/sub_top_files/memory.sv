5e8b532fe8f97415b26a0ef58e26f73df9f13dfc
charliejsampson <charliejsampson@icloud.com>
2025-12-06 00:27:59 +0000

    Modified memory top-level sheet for pipelining purposes

--- rtl/sub_top_files/memory.sv
+++ rtl/sub_top_files/memory.sv
@@ -9,6 +9,11 @@ module memory #(
     input  logic [1:0]       result_src_m_i,
     input  logic [4:0]       rd_m_i,
     input  logic [WIDTH-1:0] pc_plus4_m_i,
+
+    // forwarding inputs
+    input  logic             fwd_ls_w,
+    input  logic [WIDTH-1:0] result_w,
+
     output logic [WIDTH-1:0] read_data,
     output logic             reg_write_m_o,
     output logic [1:0]       result_src_m_o,
@@ -16,6 +21,8 @@ module memory #(
     output logic [WIDTH-1:0] pc_plus4_m_o,
     output logic [WIDTH-1:0] alu_result_m_o
 );
+
+logic [WIDTH-1:0] fwd_ls_out;
 
 assign reg_write_m_o  = reg_write_m_i;
 assign result_src_m_o = result_src_m_i;
@@ -25,10 +32,17 @@ assign alu_result_m_o = alu_result_m_i;
 
 ram data_mem (
     .adr          (alu_result_m_i),
-    .write_data   (write_data_m),
+    .write_data   (fwd_ls_out),
     .clk          (clk),
     .write_enable (mem_write_m),
     .read_data    (read_data)
+);
+
+mux_2 fwd_load_store (
+    .in0 (write_data_m),
+    .in1 (result_w),
+    .sel (fwd_ls_w),
+    .out (fwd_ls_out)
 );
 
 endmodule
